95f82ade9511e4d77c3329258fc05a6ee5f7ac04
Christian Ebert <blacktrash@gmx.net>
2016-07-14 01:34:55 +0100

    Fix build from source
    See: https://github.com/dailymotion/hls.js/pull/550

--- flowplayer.hlsjs.js
+++ flowplayer.hlsjs.js
@@ -658,7 +658,7 @@
 
     };
     if (typeof module === 'object' && module.exports) {
-        module.exports = extension.bind(undefined, require('hls.js'));
+        module.exports = extension.bind(undefined, require('hls.js/lib/hls.js'));
     } else if (window.Hls && window.flowplayer) {
         extension(window.Hls, window.flowplayer);
     }
